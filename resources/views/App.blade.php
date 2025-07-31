a419c324155ed0f7db046f2ff9f7243fcdacba45
Syaiful68 <syaifulSheva@gmail.com>
2025-07-31 14:31:56 +0700

    second commit

--- resources/views/App.blade.php
+++ resources/views/App.blade.php
@@ -6,10 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @vite(['resources/js/app.js','resources/css/app.css'])
     @inertiaHead
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.28.0/feather.min.js"></script> -->
 </head>
 
-<body>
+<body class="nav-fixed">
     @inertia
+
 </body>
 
 </html>
