f43597fd914077afa9dfa5ce6b9cf2da0036ade4
Syaiful68 <syaifulSheva@gmail.com>
2025-08-04 17:31:09 +0700

    commit

--- resources/js/Pages/Ticketing/partials/modalView.vue
+++ resources/js/Pages/Ticketing/partials/modalView.vue
@@ -1,77 +1,120 @@
 <script setup>
-const Category = [
-    { label: "Orion", name: "orion" },
-    { label: "Hybrid", name: "hybrid" },
-    { label: "Dashboard", name: "dashboard" },
-    { label: "User", name: "user" },
+import { router, usePage } from "@inertiajs/vue3";
+import { computed, reactive } from "vue";
+
+const Categories = [
+    {
+        id: 1,
+        label: "Orion",
+        name: "orion",
+        subCategori: [
+            { label: "Revisi", name: "revisi" },
+            { label: "Cancel", name: "cancel" },
+        ],
+    },
+    {
+        id: 2,
+        label: "User",
+        name: "user",
+        subCategori: [
+            { label: "Orion", name: "orion" },
+            { label: "SCA", name: "sca" },
+        ],
+    },
+    {
+        id: 3,
+        label: "Dashboard",
+        name: "dashboard",
+    },
 ];
+
+const page = usePage();
+
+const formData = reactive({
+    _token: page.props.csrf_token,
+    category: "",
+    subcategory: "",
+    description: null,
+});
+
+const subCategories = computed(() => {
+    if (formData.category) {
+        return formData.category.subCategori;
+    }
+    return [];
+});
+
+function submitTicket() {
+    router.post("/ticket", formData);
+}
 </script>
 
 <template>
-    <div
-        class="modal modal-blur"
-        id="exampleModal"
-        tabindex="-1"
-        role="dialog"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-    >
+    <div class="modal modal-blur" id="exampleModal" tabindex="-1" role="dialog">
         <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">
-                        Default Bootstrap Modal
-                    </h5>
-                    <button
-                        class="btn-close"
-                        type="button"
-                        @click="$emit('toggle')"
-                    ></button>
-                </div>
-                <div class="modal-body">
-                    <div class="mb-3">
-                        <label class="form-label">Category</label>
-                        <select class="form-control">
-                            <option value="">Choise</option>
-                            <option
-                                v-for="(item, index) in Category"
-                                :key="index"
-                                :value="item.name"
+            <form @submit.prevent="submitTicket">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">
+                            Default Bootstrap Modal
+                        </h5>
+                        <button
+                            class="btn-close"
+                            type="button"
+                            @click="$emit('toggle')"
+                        ></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label class="form-label">Category</label>
+                            <select
+                                v-model="formData.category"
+                                class="form-control"
                             >
-                                {{ item.label }}
-                            </option>
-                        </select>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Sub Category</label>
-                        <select class="form-control">
-                            <option value="">Choise</option>
-                            <option
-                                v-for="(item, index) in Category"
-                                :key="index"
-                                :value="item.name"
+                                <option value="0">Choise</option>
+                                <option
+                                    v-for="(item, index) in Categories"
+                                    :key="index"
+                                    :value="item"
+                                >
+                                    {{ item.name }}
+                                </option>
+                            </select>
+                        </div>
+                        <div class="mb-3" v-if="formData.category">
+                            <label class="form-label">Sub Category</label>
+                            <select
+                                class="form-control"
+                                v-model="formData.subcategory"
                             >
-                                {{ item.label }}
-                            </option>
-                        </select>
+                                <option value="0">Choise</option>
+                                <option
+                                    v-for="(item, index) in subCategories"
+                                    :key="index"
+                                    :value="item.name"
+                                >
+                                    {{ item.label }}
+                                </option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Description</label>
+                            <textarea class="form-control" rows="5"></textarea>
+                        </div>
                     </div>
-                    <div class="mb-3">
-                        <label class="form-label">Description</label>
-                        <textarea class="form-control" row="10"></textarea>
+                    <div class="modal-footer">
+                        <button
+                            class="btn btn-secondary"
+                            type="button"
+                            @click="$emit('toggle')"
+                        >
+                            Close</button
+                        ><button class="btn btn-primary" type="submit">
+                            Save changes
+                        </button>
                     </div>
                 </div>
-                <div class="modal-footer">
-                    <button
-                        class="btn btn-secondary"
-                        type="button"
-                        @click="$emit('toggle')"
-                    >
-                        Close</button
-                    ><button class="btn btn-primary" type="button">
-                        Save changes
-                    </button>
-                </div>
-            </div>
+            </form>
         </div>
     </div>
 </template>
